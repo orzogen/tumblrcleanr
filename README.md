98b7828e1c69ee0cd97b6ad5b502b05abee4b0fc
Dan Ruswick <orzogen@gmail.com>
2012-12-05 18:46:54 -0600

    init dedux

---
+++ README.md
@@ -0,0 +1,3 @@
+#Tumblr Cleanr
+
+Tumblr Cleanr is a crome extension that removes paid content from Tumblr, including pinned posts and Tumblr's featured sidebar.
---
+++ app/scripts/cleanr.js
@@ -0,0 +1,5 @@
+Zepto(function($){
+    $(".promotion_pinned").remove();
+    $("#tumblr_radar").remove();
+    console.log("Paid content removed from Tumblr.");
+});
